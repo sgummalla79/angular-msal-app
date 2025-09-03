616fbf01aa412c187e373d6b5f7cbe9afc8ab873
Suman Gummalla <sgummalla.work@gmail.com>
2025-09-02 20:15:39 -0700

    fixed few issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
     auth: {
       clientId: 'b312a0ef-4376-43b9-866f-b1baf02e84d7', // Same as development
       authority: 'https://login.microsoftonline.com/consumers',
-      redirectUri: 'angular-msal-app.azurewebsites.net', // Update this
-      postLogoutRedirectUri: 'angular-msal-app.azurewebsites.net'
+      redirectUri: 'https://angular-msal-app.azurewebsites.net', // Update this
+      postLogoutRedirectUri: 'https://angular-msal-app.azurewebsites.net'
     }
   }
 };
